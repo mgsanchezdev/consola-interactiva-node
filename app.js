c4c17fa806f109e40b868e80e4a3c42df24c2aee
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-19 17:34:24 -0300

    Build the interactive menu-Inquirer

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 require("colors");
-const { mostrarMenu, pausa } = require("./helpers/mensajes");
+const { inquirerMenu } = require("./helpers/inquire");
 
-console.clear();
+// console.clear();
 const main = async () => {
   let opt = "";
   do {
-    opt = await mostrarMenu();
+    opt = await inquirerMenu();
     console.log({ opt });
 
-    if (opt !== '0') await pausa();
   } while (opt !== "0");
 };
 
