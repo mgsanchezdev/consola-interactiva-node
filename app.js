b62bf122c71b225eeea57e2f04f9b3159fbe5694
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-19 16:11:46 -0300

    Stdin. stdout, Readline

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("colors");
+const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 console.clear();
-const main = async () => {};
-console.log("Hola mundo");
+const main = async () => {
+    console.log("Hola mundo");
+    mostrarMenu();
+  //  pausa();
+};
+
 
 main();
