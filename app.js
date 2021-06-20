6c450dc8b22ed2e3b9599c5c2641911c5007b158
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-06-19 21:21:43 -0300

    Create and list tasks

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 require("colors");
-const { inquirerMenu, pausa } = require("./helpers/inquire");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquire");
 const Tareas = require("./helpers/models/tareas");
 
 const main = async () => {
   let opt = "";
+  const tareas = new Tareas();
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+    switch (opt) {
+      case "1":
+        //create option
+        const desc = await leerInput(" Descripcion:");
+        tareas.crearTarea(desc);
+        break;
+      case "2":
+        console.log(tareas._listado);
+        break;
+    }
 
     await pausa();
   } while (opt !== "0");
